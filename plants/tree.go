484538540ea2bcfc407f03ffedc2a4e5674f3d7a
John Ryan <jtigger@infosysengr.com>
2020-11-25 15:04:16 -0800

    Stringify a Tree.

--- plants/tree.go
+++ plants/tree.go
@@ -1,8 +1,14 @@
 package plants
 
+import "fmt"
+
 type Tree struct {
 	Name string
 	Height int
+}
+
+func (t *Tree) String() string {
+	return fmt.Sprintf("Tree \"%s\" standing %d inches tall.", t.Name, t.Height)
 }
 
 func NewTree(name string) Tree {
