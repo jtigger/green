7efc59272eb065b96903e755bda0e787620c027e
John Ryan <jtigger@infosysengr.com>
2020-11-25 16:58:28 -0800

    Rename "Tree" to "Arbor"

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jtigger/green
+module github.com/jtigger/green/v2
 
 go 1.14
--- plants/tree.go
+++ plants/tree.go
@@ -2,17 +2,17 @@ package plants
 
 import "fmt"
 
-type Tree struct {
+type Arbor struct {
 	Name string
 	Height int
 	Width int
 }
 
-func (t *Tree) String() string {
-	return fmt.Sprintf("Tree \"%s\" standing %d inches tall and %d wide.", t.Name, t.Height, t.Width)
+func (t *Arbor) String() string {
+	return fmt.Sprintf("Arbor \"%s\" standing %d inches tall and %d wide.", t.Name, t.Height, t.Width)
 }
 
-func NewTree(name string, height int) Tree {
-	return Tree{Name: name, Height: height, Width: 12}
+func NewArbor(name string, height int) Arbor {
+	return Arbor{Name: name, Height: height, Width: 12}
 }
 
